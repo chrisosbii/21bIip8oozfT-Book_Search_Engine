b25a2197e8e880f2e18fd386b13628b68f8f7fc4
Chris Osborne <chrisosbii@gmail.com>
2023-08-08 23:00:25 -0500

    added queries.js and fixed resolver query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,18 +5,7 @@ export const GET_ME = gql`
     me {
       _id
       name
-    }
-  }
-`;
-
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      savedBooks
     }
   }
 `;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,11 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const resolvers = {
   Query: {
     // get the current user
-    me: async (parent, { username }) => {
-      return User.findOne({ username });
+    me: async (parent, ags, context) => {
+        if (context.user) {
+            return User.findOne({ _id: context.user._id });
+        }
+        throw AuthenticationError;
     },
   },
   Mutation: {
